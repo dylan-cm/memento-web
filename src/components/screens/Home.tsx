9a8bbd62af348a9b5508b92caabca3b633acb1c3
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-03-15 17:31:59 -0700

    headers, fonts, and adobe typekit

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -6,7 +6,15 @@ const Home = () => {
   return (
     <Layout>
       <Section id="hero">
-      Hero Section
+      <h1>Hero Section</h1>
+      <h2>Hero Section</h2>
+      <h3>Hero Section</h3>
+      <h4>Hero Section</h4>
+      <h5>Hero Section</h5>
+      <h6>Hero Section</h6>
+      <p>Hero Section</p>
+      <em>Hero Section</em>
+      <strong>Hero Section</strong>
       </Section>
       <Section id="value-prop">
       Value Proposition Section
@@ -18,7 +26,7 @@ const Home = () => {
       How It Works Section
       </Section>
       <Section id="download-app">
-      Dowload the App Section
+      Download the App Section
       </Section>
     </Layout>
   );
